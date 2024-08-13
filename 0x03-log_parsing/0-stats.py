d325b24397c101dca07f8e183ab3261823e81924
MGHefny <mgenedhefny@gmail.com>
2024-08-13 21:50:57 +0300

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
+"""ead stdin line by line"""
 
 import sys
 
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+status_case = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+length = 0
+length_count = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
+        part_list = line.split(" ")
+        if len(part_list) > 4:
+            st_code = part_list[-2]
+            list_siz = int(part_list[-1])
+            if st_code in status_case:
+                status_case[st_code] += 1
+            length += list_siz
+            length_count += 1
 
-        stlist = line.split()
-        count += 1
+        if length_count == 10:
+            length_count = 0
+            print(f"File list_siz: {length}")
+            for case_num, case_size in sorted(status_case.items()):
+                if case_size > 0:
+                    print(f"{case_num}: {case_size}")
+except Exception as error:
+    pass
 
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+if __name__ == '__main__':
+    print(f"File size: {length}")
+    for case_num, case_size in sorted(status_case.items()):
+        if case_size > 0:
+            print(f"{case_num}: {case_size}")
