951fb59352b6e7b03e4a8b75172dc4e26ac1fa68
MGHefny <mgenedhefny@gmail.com>
2024-08-15 19:57:25 +0300

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""""""
+"""ead stdin line by line"""
+
 import sys
 
 status_case = {
@@ -15,30 +16,25 @@ status_case = {
 length = 0
 length_count = 0
 
-
 try:
     for line in sys.stdin:
         part_list = line.split(" ")
         if len(part_list) > 4:
             st_code = part_list[-2]
-            try:
-                list_siz = int(part_list[-1])
-            except ValueError:
-                continue
-
+            list_siz = int(part_list[-1])
             if st_code in status_case:
                 status_case[st_code] += 1
-
             length += list_siz
             length_count += 1
-            if length_count == 10:
-                print(f"File list_siz: {length}")
-                for case_num, case_size in sorted(status_case.items()):
-                    if case_size > 0:
-                        print(f"{case_num}: {case_size}")
-                length_count = 0
+
+        if length_count == 10:
+            length_count = 0
+            print(f"File list_siz: {length}")
+            for case_num, case_size in sorted(status_case.items()):
+                if case_size > 0:
+                    print(f"{case_num}: {case_size}")
 except Exception as error:
-    print(f"An error occurred: {error}")
+    pass
 
 if __name__ == "__main__":
     print(f"File size: {length}")
