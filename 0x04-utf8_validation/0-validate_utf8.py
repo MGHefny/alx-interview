ced10a0a821f4b4e2d9c298bd4231413336952f1
MGHefny <mgenedhefny@gmail.com>
2024-08-22 19:32:26 +0300

    0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Task 0 UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    return the int of list valid utf-8
+    the count of the byte or true or fals
+    """
+    co_byte = 0
+
+    for x in data:
+        # Check the number of leading 1s
+        if co_byte == 0:
+            """char encod utf-8 3 byte"""
+            if x >> 3 == 0b11110:
+                co_byte = 3
+                """char encod utf-8 2 byte"""
+            elif x >> 4 == 0b1110:
+                co_byte = 2
+                """char encod utf-8 1 byte"""
+            elif x >> 5 == 0b110:
+                co_byte = 1
+                """char encod utf-8 fals"""
+            elif x >> 7 == 0b1:
+                return False
+        else:
+            """cant count byte"""
+            if x >> 6 != 0b10:
+                return False
+            co_byte -= 1
+
+    return co_byte == 0
