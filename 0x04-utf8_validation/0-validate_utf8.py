57094c849f8c559f717c245a33c88c465b3099f5
MGHefny <mgenedhefny@gmail.com>
2024-08-22 19:37:12 +0300

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,15 +14,15 @@ def validUTF8(data):
     for x in data:
         # Check the number of leading 1s
         if co_byte == 0:
-            """char encod utf-8 3 byte"""
-            if x >> 3 == 0b11110:
-                co_byte = 3
+            """char encod utf-8 1 byte"""
+            if x >> 5 == 0b110:
+                co_byte = 1
                 """char encod utf-8 2 byte"""
             elif x >> 4 == 0b1110:
                 co_byte = 2
-                """char encod utf-8 1 byte"""
-            elif x >> 5 == 0b110:
-                co_byte = 1
+                """char encod utf-8 3 byte"""
+            elif x >> 3 == 0b11110:
+                co_byte = 3
                 """char encod utf-8 fals"""
             elif x >> 7 == 0b1:
                 return False
