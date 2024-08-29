72f76cda718ca5935edf08145c9b8f3ab1c46330
MGHefny <mgenedhefny@gmail.com>
2024-08-29 21:09:03 +0300

    0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -24,31 +24,21 @@ def n_queen(a, q, c, result):
                 n_queen(a, q, c + 1, result)
 
 
-def master():
-    """ this the main function to main progress """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    N = sys.argv[1]
-
-    try:
-        N = int(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    q = [0] * (N + 1)
-    result = []
-    n_queen(N, q, 1, result)
-
-    for sol in result:
-        print(sol)
-
-
-if __name__ == "__main__":
-    master()
+""" this the main function to main progress """
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+N = sys.argv[1]
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+q = [0] * (N + 1)
+result = []
+n_queen(N, q, 1, result)
+for sol in result:
+    print(sol)
