353757ec07505c00213bcad0dbf3940a6c205785
MGHefny <mgenedhefny@gmail.com>
2024-09-12 19:32:21 +0300

    0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,11 +7,10 @@ def rotate_2d_matrix(matrix):
     """ rotate 2d matrix n*n
     """
     n = len(matrix[0])
-    b = matrix[y][x], matrix[x][y]
 
     for x in range(n):
         for y in range(x, n):
-            matrix[x][y], matrix[y][x] = b
+            matrix[x][y], matrix[y][x] = matrix[y][x], matrix[x][y]
 
     for x in range(n):
         matrix[x].reverse()
